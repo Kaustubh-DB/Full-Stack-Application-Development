22add5a005abdcf1e6d15c8adfa47131eb5ff655
Raj Nandu <rajnandu2711@gmail.com>
2019-12-12 02:14:12 -0500

    End_point to create an assignment by parsing the json file

--- blueprint/assignment/routes.py
+++ blueprint/assignment/routes.py
@@ -15,7 +15,40 @@ db = client['heroku_kx6px18d']
 def create_assignment():
     file = request.files['assignment'].read()
     data = json_util.loads(file)
-    db.assignments.insert_one(data)
+    teams = data['teams']
+    data['teams'] = []
+    for team in teams:
+        jira_data = {
+            "jira_key":  team['jira_key']            
+        }
+        jira_id = db.jira.insert(jira_data)
+
+        github_data = {
+            "github_username":  team['github_username'],     
+            "github_repository_name": team['github_repository_name']       
+        }
+        github_id = db.github.insert(github_data)
+
+        bamboo_data = {
+            "bamboo_key":  team['bamboo_key'],
+            "bamboo_plan_key": team["bamboo_plan_key"]         
+        }
+        bamboo_id = db.bamboo.insert(bamboo_data)
+
+        sonarqube_data = {
+            "sonarqube_key":  team['sonarqube_key']            
+        }
+        sonarqube_id = db.sonarqube.insert(sonarqube_data)
+        team_data = {
+            "team_name": team['team_name'],
+            'jira_id': str(jira_id),
+            'bamboo_id': str(bamboo_id),
+            'sonarqube_id': str(sonarqube_id),
+            'github_id': str(github_id)
+        }
+        data['teams'].append(team_data)
+    print(data)
+    db.assignments.insert(data)
     return "Hi"
 
 @jira.route("/assignments", methods = ['GET'])
